d6155edfe4310508ad98a7b54a5dc8ea9f7c665d
akhila2711 <akhilakhi112799@gmail.com>
2025-05-08 15:50:12 +0530

    add app

--- src/customerView.js
+++ src/customerView.js
@@ -4,9 +4,9 @@ export default function CustomerView({ customers }) {
   return (
     <>
       <h3>Customer List</h3>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {customers.map((member, index) => (
-          <li key={index}>{member}</li>
+      <ul style={{ listStyleType: 'none'}}>
+        {customers.map((member) => (
+          <li>{member}</li>
         ))}
       </ul>
     </>
---
+++ src/slices/customerSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = []
+
+
+const CustomerSlice = createSlice({
+    name: 'customer',
+    reducers:{
+        addCustomer(state, action) {
+    }
+}
+    
+})
